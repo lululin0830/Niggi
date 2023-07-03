640ab13ebc26a9a38d8605024c05ab7f9d7d92e0
Lulu <lululin0830@gmail.com>
2023-07-03 14:47:30 +0800

    group version developer enviroment setup

---
+++ src/main/java/test/controller/TestController.java
@@ -0,0 +1,24 @@
+package test.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/TestController")
+public class TestController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String name = request.getParameter("name");
+		request.setAttribute("nickname", name);
+		request.getRequestDispatcher("/index.jsp").forward(request, response);
+		
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
